120ce971cedea184576cae9476e351d640ac8016
Sergio Borbone <sergioborbone@gmail.com>
2022-02-25 19:05:37 +0100

    Creato header con searchbar

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+    <myHeader></myHeader>
+
+  </div>
+</template>
+
+<script>
+
+import myHeader from './components/myHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    myHeader,
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './assets/style/general.scss';
+</style>
---
+++ src/components/myHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="header">
+
+    <div class="logo">
+        <h1>Boolflix</h1>
+    </div>
+
+    <div class="search">
+      <input type="search" placeholder="Cerca">
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+
+  @import '../assets/style/header.scss';
+
+</style>
