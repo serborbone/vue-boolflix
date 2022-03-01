a9412e6c8a3fcda7090d5d2fa87ccd893bc9c7ca
Sergio Borbone <sergioborbone@gmail.com>
2022-03-01 20:10:25 +0100

    Milestone 4 completato

--- src/assets/partials/cardMovie.vue
+++ src/assets/partials/cardMovie.vue
@@ -1,19 +1,33 @@
 <template>
   <ul>
 
-    <li v-for="(movies, index) in moviesData" :key="index">
-        <!-- IMG FILM -->
-        <div class="movie-img">
-          <img :src="'https://image.tmdb.org/t/p/w185' + movies.poster_path">
+    <li class="card" v-for="(movies, index) in moviesData" :key="index">
+        <div class="card-inner">
+          <!-- IMG FILM -->
+          <div class="card-front">
+            <img v-if="movies.poster_path" :src="'https://image.tmdb.org/t/p/w342' + movies.poster_path">
+            <img v-else class="else-img" src="../style/img/img-else.jpg">
+          </div>
+          
+          <div class="card-back">
+            <div class="info-wrap">
+              <p><span>Titolo:</span> {{movies.title}}</p>
+              <p><span>Titolo originale:</span>  {{movies.original_title}}</p>
+              <p><span>Lingua originale:</span> <lang-flag :iso="movies.original_language" /> </p>
+              <p>
+                <span>Voto:</span>
+                <i v-for="i in 5" :key="i" class="star-color fa-star" :class="(i < getVoteInt(index)) ? 'fa-solid' : 'fa-regular' "></i>
+              </p>
+
+              <p :class="movies.overview? '' : 'd-none' ">
+                <span>Trama:</span> 
+                {{movies.overview}}
+              </p>
+              
+            </div>
+          </div>
+
         </div>
-        
-        <p><span>Titolo:</span> {{movies.title}}</p>
-        <p><span>Titolo originale:</span>  {{movies.original_title}}</p>
-        <p><span>Lingua originale:</span> <lang-flag :iso="movies.original_language" /> </p>
-        <p>
-          <span>Voto:</span>
-          <i v-for="i in 5" :key="i" class="fa-star" :class="(i < getVoteInt(index)) ? 'fa-solid' : 'fa-regular' "></i>
-        </p>
     </li>
 
   </ul>
@@ -43,5 +57,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import '../style/main.scss';
+  @import '../style/cardstyle.scss';
 </style>
--- src/assets/partials/cardSeries.vue
+++ src/assets/partials/cardSeries.vue
@@ -1,19 +1,34 @@
 <template>
   <ul>
 
-    <li class="series" v-for="(series, index) in seriesData" :key="index">
-        <!-- IMG SERIE -->
-        <div class="movie-img">
-          <img :src="'https://image.tmdb.org/t/p/w185' + series.poster_path">
+    <li class="card" v-for="(series, index) in seriesData" :key="index">
+        <div class="card-inner">
+          <!-- IMG SERIE -->
+          <div class="card-front">
+            <img v-if="series.poster_path" :src="'https://image.tmdb.org/t/p/w342' + series.poster_path">
+            <img v-else class="else-img" src="../style/img/img-else.jpg">
+          </div>
+          
+          <div class="card-back">
+            <div class="info-wrap">
+
+              <p> <span>Titolo:</span> {{series.name}} </p>
+              <p> <span>Titolo originale:</span> {{series.original_name}} </p>
+              <p> <span>Lingua originale:</span> <lang-flag :iso="series.original_language" /> </p>
+              <p>
+                <span>Voto:</span>
+                <i v-for="i in 5" :key="i" class="star-color fa-star" :class="(i < getVoteInt(index)) ? 'fa-solid' : 'fa-regular' "></i>
+              </p>
+
+              <p :class="series.overview? '' : 'd-none' ">
+                <span>Trama:</span> 
+                {{series.overview}}
+              </p>
+
+            </div>
+          </div>
+
         </div>
-        
-        <p> <span>Titolo:</span> {{series.name}} </p>
-        <p> <span>Titolo originale:</span> {{series.original_name}} </p>
-        <p> <span>Lingua originale:</span> <lang-flag :iso="series.original_language" /> </p>
-        <p>
-          <span>Voto:</span>
-          <i v-for="i in 5" :key="i" class="fa-star" :class="(i < getVoteInt(index)) ? 'fa-solid' : 'fa-regular' "></i>
-        </p>
     </li>
 
   </ul>
@@ -42,5 +57,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import '../style/main.scss';
+  @import '../style/cardstyle.scss';
 </style>
